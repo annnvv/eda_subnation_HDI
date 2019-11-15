d38847f3ea7d65af4a5266443d81cb0d345a857c
annnvv <13822260+annnvv@users.noreply.github.com>
2019-11-15 01:27:23 -0500

    change global object to national object

--- scripts/03_global_reshape_data.R
+++ scripts/03_global_reshape_data.R
@@ -1,46 +1,46 @@
-  # read in the globalambique National and Subnational HDI data from 1990 to 2017
-  global <- read.csv(paste0(proj_path, "/data/GDL-national-HDI-data.csv"))
-  head(global)  
+  # read in the nationalambique National and Subnational HDI data from 1990 to 2017
+  national <- read.csv(paste0(proj_path, "/data/GDL-national-HDI-data.csv"))
+  head(national)  
   
   # add an y_ to year vars
-  names(global) <- gsub("^X", "y_", names(global))  
+  names(national) <- gsub("^X", "y_", names(national))  
   
   # can remove unnecessary columns
-  global <- global[ c(1,2,6:length(global))]
-  names(global)
+  national <- national[ c(1,2,6:length(national))]
+  names(national)
   
   # left merge in regions by ISO3 code
   iso3_regions <- read.csv("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv")
   iso3_regions <- iso3_regions[ , c(3,6)]
   names(iso3_regions)[1] <- "ISO_Code"
   
-  global <- merge(global, iso3_regions, by = "ISO_Code", all.x = TRUE, all.y = FALSE)
+  national <- merge(national, iso3_regions, by = "ISO_Code", all.x = TRUE, all.y = FALSE)
   
-  global <- global[ ,c(1,2,length(global), 3:30)]
+  national <- national[ ,c(1,2,length(national), 3:30)]
   
-  global$region[global$Country == "Kosovo"] <- "Europe"
+  national$region[national$Country == "Kosovo"] <- "Europe"
   
   #need to reshape the data to be long instead of wide!
-  global_long <- reshape(global, 
-                      varying = c(names(global)[4:31]), 
+  national_long <- reshape(national, 
+                      varying = c(names(national)[4:31]), 
                       v.names = "HDI",
                       timevar = "year", 
-                      times = c(names(global)[4:31]), 
+                      times = c(names(national)[4:31]), 
                       new.row.names = 1:4508,
                       direction = "long")
   
   #review the structure of reshaped dataframe
-  str(global_long)
+  str(national_long)
   
   # replace Total with Country (to indicate the country HDI score)
-  global_long$Country <- as.character(global_long$Country)
-  global_long$ISO_Code <- as.character(global_long$ISO_Code)
+  national_long$Country <- as.character(national_long$Country)
+  national_long$ISO_Code <- as.character(national_long$ISO_Code)
   
   # remove y_ from year and make numeric
-  global_long$year <- as.character(global_long$year)
-  global_long$year <- as.numeric(gsub("y_", "", global_long$year))
+  national_long$year <- as.character(national_long$year)
+  national_long$year <- as.numeric(gsub("y_", "", national_long$year))
   
   # save cleaned data
-  write.csv(global_long, paste0(proj_path, "/data/global_long_HDI.csv"), 
+  write.csv(national_long, paste0(proj_path, "/data/national_long_HDI.csv"), 
             row.names = FALSE)
   
