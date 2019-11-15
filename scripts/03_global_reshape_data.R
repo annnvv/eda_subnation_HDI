c9f824ef3fa933f768ed771b904d1088ed120a58
annnvv <13822260+annnvv@users.noreply.github.com>
2019-11-15 01:22:47 -0500

    added regions to the data

--- scripts/03_global_reshape_data.R
+++ scripts/03_global_reshape_data.R
@@ -9,12 +9,23 @@
   global <- global[ c(1,2,6:length(global))]
   names(global)
   
+  # left merge in regions by ISO3 code
+  iso3_regions <- read.csv("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv")
+  iso3_regions <- iso3_regions[ , c(3,6)]
+  names(iso3_regions)[1] <- "ISO_Code"
+  
+  global <- merge(global, iso3_regions, by = "ISO_Code", all.x = TRUE, all.y = FALSE)
+  
+  global <- global[ ,c(1,2,length(global), 3:30)]
+  
+  global$region[global$Country == "Kosovo"] <- "Europe"
+  
   #need to reshape the data to be long instead of wide!
   global_long <- reshape(global, 
-                      varying = c(names(global)[3:30]), 
+                      varying = c(names(global)[4:31]), 
                       v.names = "HDI",
                       timevar = "year", 
-                      times = c(names(global)[3:30]), 
+                      times = c(names(global)[4:31]), 
                       new.row.names = 1:4508,
                       direction = "long")
   
@@ -30,5 +41,6 @@
   global_long$year <- as.numeric(gsub("y_", "", global_long$year))
   
   # save cleaned data
-  write.csv(global_long, paste0(proj_path, "/data/global_long_HDI.csv"), row.names = FALSE)
+  write.csv(global_long, paste0(proj_path, "/data/global_long_HDI.csv"), 
+            row.names = FALSE)
   
